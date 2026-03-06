7f31652acca0890e7e4c3614ba0b053a653b3770
Marcelo Parabocz <macerno@gmail.com>
2026-03-06 15:31:43 -0300

    feat: partial initial placement implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,7 @@ import chess.ChessMatch;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("This is almost working already /s");
+		System.out.println("This is almost working already /s\n");
 
 		ChessMatch match = new ChessMatch();
 		
