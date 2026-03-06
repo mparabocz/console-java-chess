4d04755938d3bbea86476e192483ad77b0c32839
Marcelo Parabocz <macerno@gmail.com>
2026-03-06 15:07:38 -0300

    feat: board and piece starting implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,13 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("This is almost working already /s");
 
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		Board board = new Board(8,8);
 
 	}
 
